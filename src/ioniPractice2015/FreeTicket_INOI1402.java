51dcd6ad51eee80cd0f66c7db13323ffd0586b31
K81094 <kamal81094@gmail.com>
2017-08-04 21:36:05 +0530

    INOI problem 1 initial commit

---
+++ src/ioniPractice2015/FreeTicket_INOI1402.java
@@ -0,0 +1,25 @@
+package ioniPractice2015;
+/*
+ * Problem Name : Free Ticket
+ * Problem Id : INOI1402
+ * Attempt required : 0
+ * Url : https://www.codechef.com/IOIPRAC/problems/INOI1402
+ * Date : 04 08 2017
+ *
+ */
+public class FreeTicket_INOI1402 {
+
+	public static void main(String[] args) {
+		int m=5;
+		int n=5;
+		int[][]a=new int[m][n];
+		for(int i=0;i<m;i++){
+			for(int j=0;j<m;j++){
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
