03dde23653b0dec3f03bff07ca532e65d2f4245d
K81094 <kamal81094@gmail.com>
2017-08-04 23:26:48 +0530

    Partial test case ppassed for free ticket

--- src/ioniPractice2015/FreeTicket_INOI1402.java
+++ src/ioniPractice2015/FreeTicket_INOI1402.java
@@ -1,4 +1,7 @@
 package ioniPractice2015;
+
+import java.util.Scanner;
+
 /*
  * Problem Name : Free Ticket
  * Problem Id : INOI1402
@@ -10,16 +13,69 @@ package ioniPractice2015;
 public class FreeTicket_INOI1402 {
 
 	public static void main(String[] args) {
-		int m=5;
-		int n=5;
-		int[][]a=new int[m][n];
-		for(int i=0;i<m;i++){
-			for(int j=0;j<m;j++){
-				System.out.print(a[i][j]+" ");
-			}
-			System.out.println();
+		Scanner sc=new Scanner(System.in);
+		
+		int c=sc.nextInt();
+		int f=sc.nextInt();
+		int[][]a=new int[c+1][c+1];
+		int[][]dp=a;
+		int result=-1;
+		for(int i=1;i<=f;i++){
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			int z=sc.nextInt();
+			a[x][y]=z;
+			dp[x][y]=z;
 		}
 		
+		//calculate dp
+		for(int i=1;i<=c;i++){
+			
+			
+			
+			for(int j=1;j<=c;j++){
+				//System.out.println(dp[i-1][j]+" "+dp[i-1][j-1]+" "+dp[i][j-1]+" = "+findMinOfThree(dp[i-1][j],dp[i-1][j-1],dp[i][j-1]));
+				//System.out.println(dp[i][j] +" "+a[i][j]);
+				if(dp[i][j]!=0){
+					dp[i][j]=dp[i][j]+findMinOfThree(dp[i-1][j],dp[i-1][j-1],dp[i][j-1]);
+					result=dp[i][j];
+				}				
+			}
+		}
+		System.out.println(result); //5 test case passout of 20
+		//System.out.println(dp[c-1][c]);//2 test case passout of 20
+		
+		
 	}
+
+	public static int findMinOfThree(int x,int y,int z){
+		
+		if(x!=0 && y!=0 &&  z!=0){
+			if (x < y)
+	            return (x < z)? x : z;
+	        else
+	            return (y < z)? y : z;
+		}else if(x==0 && y!=0 &&  z!=0){			
+	            return (y < z)? y : z;
+		}else if(x!=0 && y==0 &&  z!=0){			
+            return (x < z)? x : z;
+		}else if(x!=0 && y!=0 &&  z==0){			
+            return (x < y)? x : y;
+		}else if(x!=0 && y==0 &&  z==0){			
+            return x;
+		}else if(x==0 && y!=0 &&  z==0){			
+            return y;
+		}else if(x==0 && y==0 &&  z!=0){			
+            return z;
+		}else {
+			if (x < y)
+	            return (x < z)? x : z;
+	        else
+	            return (y < z)? y : z;
+		}
+		
+		
+	}
+	
 
 }
